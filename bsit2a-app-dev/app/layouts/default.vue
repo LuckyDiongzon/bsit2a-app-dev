c7be72127b9e695b7fa7ce95d2fa1dd93ba9ff38
itsjantquinto <itsjantquinto@gmail.com>
2026-02-03 10:24:52 +0800

    Added layout

--- bsit2a-app-dev/app/layouts/default.vue
+++ bsit2a-app-dev/app/layouts/default.vue
@@ -1,10 +1,47 @@
 <template>
   <v-app>
-    <slot />
+  <v-app-bar :elevation="2">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon @click="drawer = !drawer; rail = !rail"></v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+</v-app-bar>
+
+    <v-navigation-drawer v-model=" drawer" 
+    :rail="rail"
+    permanent>
+      
+        <template v-slot:prepend>
+          <v-list-item
+            lines="two"
+            prepend-avatar=c:\Users\SNCBT\Documents\607158395_1876656183221085_2326868583434191434_n.jpg
+            subtitle="Logged in"
+            title="Adrian Diongzon"
+          ></v-list-item>
+        </template>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    <v-main>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+
   </v-app>
 </template>
 
-<script lang="ts" setup>
+<script setup>
+import { ref } from 'vue'
+const drawer = ref(true);
+const rail = ref(true)
 
 </script>
 
--- bsit2a-app-dev/app/pages/index.vue
+++ bsit2a-app-dev/app/pages/index.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
-    <v-btn>submit</v-btn>
+    
+    <v-row>
+      <v-col cols="12" mid="2">
+        <v-card height="90" color="primary"></v-card>
+      </v-col>
+      <v-col cols="12" mid="2">
+          <v-card height="90" color="blue"></v-card>
+      </v-col>
+      <v-col cols="12" mid="2">
+        <v-card height="90" color="green"></v-card>
+      </v-col>
+      <v-col cols="12" mid="2">
+        <v-card height="90" color="orange"></v-card>
+      </v-col>
+    </v-row>
+
   </div>
 </template>
 
 <script lang="ts" setup>
-
 </script>
 
 <style>
-
 </style>
